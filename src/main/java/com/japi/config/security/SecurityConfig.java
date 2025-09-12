c72d774ad5ecb1458c83de379ef3d242cd6a25e0
jhu900202 <jhu900202@outlook.com>
2025-09-13 02:05:20 +0900

    시큐리티 설정(작업중)

--- src/main/java/com/japi/config/security/SecurityConfig.java
+++ src/main/java/com/japi/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.japi.config.security;
 
+import com.japi.config.authentication.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
